750a44cb260516a82225143dc892d0f9275252a3
Keijiro Takahashi <keijiro@gmail.com>
2017-09-25 16:08:53 -0700

    Changed to use OnRenderObject

--- Assets/InstancedMeshDrawer.cs
+++ Assets/InstancedMeshDrawer.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
-using System.Collections.Generic;
 
 [ExecuteInEditMode]
 public class InstancedMeshDrawer : MonoBehaviour
@@ -16,20 +15,13 @@ public class InstancedMeshDrawer : MonoBehaviour
 
     // Temporary objects for instanced rendering
     Material _material;
-    MaterialPropertyBlock _materialProps;
     ComputeBuffer _drawArgsBuffer;
-
-    // Huge bounding box
-    readonly Bounds kBounds = new Bounds(Vector3.zero, Vector3.one * 1000);
 
     // Command buffer used for rendering motion vectors
     CommandBuffer _motionVectorsPass;
 
-    // Camera event used for rendering motion vectors
-    const CameraEvent kMotionVectorsEvent = CameraEvent.BeforeImageEffectsOpaque;
-
-    // Cameras that the motion vectors pass has been added to
-    HashSet<Camera> _cameras = new HashSet<Camera>();
+    // Huge bounding box
+    readonly Bounds kBounds = new Bounds(Vector3.zero, Vector3.one * 10000);
 
     #endregion
 
@@ -37,12 +29,6 @@ public class InstancedMeshDrawer : MonoBehaviour
 
     void OnDisable()
     {
-        // Remove the motion vector pass from the cameras.
-        foreach (var camera in _cameras)
-            camera.RemoveCommandBuffer(kMotionVectorsEvent, _motionVectorsPass);
-
-        _cameras.Clear();
-
         // We have to release compute buffers not in OnDestory but in
         // OnDisable to avoid buffer leakage warning.
         if (_drawArgsBuffer != null)
@@ -61,15 +47,9 @@ public class InstancedMeshDrawer : MonoBehaviour
             else
                 DestroyImmediate(_material);
         }
-    }
 
-    void OnRenderObject()
-    {
         if (_motionVectorsPass != null)
-        {
-            if ((Camera.current.depthTextureMode & DepthTextureMode.MotionVectors) != 0)
-                Graphics.ExecuteCommandBuffer(_motionVectorsPass);
-        }
+            _motionVectorsPass.Dispose();
     }
 
     void Update()
@@ -80,16 +60,17 @@ public class InstancedMeshDrawer : MonoBehaviour
         _material.SetFloat("_DeltaTime", Time.deltaTime);
         _material.SetMatrix("_LocalToWorld", transform.localToWorldMatrix);
         _material.SetMatrix("_WorldToLocal", transform.worldToLocalMatrix);
-        /*
-        _materialProps.SetFloat("_CurrentTime", Application.isPlaying ? Time.time : 0);
-        _materialProps.SetFloat("_DeltaTime", Time.deltaTime);
-        _materialProps.SetMatrix("_LocalToWorld", transform.localToWorldMatrix);
-        _materialProps.SetMatrix("_WorldToLocal", transform.worldToLocalMatrix);
-        */
 
-        Graphics.DrawMeshInstancedIndirect(_mesh, 0, _material, kBounds, _drawArgsBuffer, 0, _materialProps);
+        Graphics.DrawMeshInstancedIndirect(_mesh, 0, _material, kBounds, _drawArgsBuffer);
+    }
 
-        UpdateAllCameras();
+    void OnRenderObject()
+    {
+        if (_motionVectorsPass != null &&
+            (Camera.current.depthTextureMode & DepthTextureMode.MotionVectors) != 0)
+        {
+            Graphics.ExecuteCommandBuffer(_motionVectorsPass);
+        }
     }
 
     #endregion
@@ -104,9 +85,6 @@ public class InstancedMeshDrawer : MonoBehaviour
             _material.hideFlags = HideFlags.DontSave;
         }
 
-        if (_materialProps == null)
-            _materialProps = new MaterialPropertyBlock();
-
         if (_drawArgsBuffer == null)
         {
             _drawArgsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
@@ -116,34 +94,9 @@ public class InstancedMeshDrawer : MonoBehaviour
         if (_motionVectorsPass == null)
         {
             _motionVectorsPass = new CommandBuffer();
-            _motionVectorsPass.name = "MotionVectors";
+            _motionVectorsPass.name = "Motion Vectors";
             _motionVectorsPass.SetRenderTarget(BuiltinRenderTextureType.MotionVectors);
-            _motionVectorsPass.DrawMeshInstancedIndirect(_mesh, 0, _material, 0, _drawArgsBuffer, 0, _materialProps);
-        }
-    }
-
-    void UpdateAllCameras()
-    {
-        foreach (var camera in Camera.allCameras)
-        {
-            if ((camera.depthTextureMode & DepthTextureMode.MotionVectors) != 0)
-            {
-                /*
-                if (!_cameras.Contains(camera))
-                {
-                    camera.AddCommandBuffer(kMotionVectorsEvent, _motionVectorsPass);
-                    _cameras.Add(camera);
-                }
-                */
-            }
-            else
-            {
-                if (_cameras.Contains(camera))
-                {
-                    camera.RemoveCommandBuffer(kMotionVectorsEvent, _motionVectorsPass);
-                    _cameras.Remove(camera);
-                }
-            }
+            _motionVectorsPass.DrawMeshInstancedIndirect(_mesh, 0, _material, 0, _drawArgsBuffer);
         }
     }
 
